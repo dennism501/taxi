d59847566b6b04a2902a3ca3b0ec8b469ad45dd1
Dennis Mubamba <dennism501@outlook.com>
2018-06-02 14:14:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", "192.168.170.135");
-define("DB_USER", "root");
-define("DB_PASSWORD", "843663");
-define("DB_DATABASE", "gudenergy");
+define("DB_HOST", "ip address");
+define("DB_USER", "username");
+define("DB_PASSWORD", "password");
+define("DB_DATABASE", "dbname");
 ?>
